f44e7c6f19bd847f00eb0c941b3993c73ceae1cf
rameyrobo <rameyrobo@gmail.com>
2025-06-28 07:36:08 -0700

    Updated mobile spacing on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className="flex flex-wrap gap-4 mt-4 text-xl font-semibold uppercase flex-col md:flex-row items-center">
+    <div className="flex flex-wrap gap-1 mt-4 text-xl font-semibold uppercase flex-col md:flex-row md:gap-4 items-center">
             <a href="#story-section" className="text-2xl px-5 hover:underline scroll-link">MF’s Story</a>
             <a href="#be-mf" className="text-2xl px-5 hover:underline">Be a MF</a>
             <a href="#contact" className="ext-2xl px-5 hover:underline">Contact</a>
