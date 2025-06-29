58cad404f81fe6d5e801657526223952901714cb
rameyrobo <rameyrobo@gmail.com>
2025-06-28 17:10:19 -0700

    Updated letter-spacing styles in the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 export default function Navbar() {
   return (
     <div className="flex flex-wrap gap-1 mt-4 text-xl font-semibold uppercase flex-col md:flex-row md:gap-4 items-center">
-            <a href="#story-section" className="text-2xl px-5 hover:underline scroll-link">MF’s Story</a>
-            <a href="#be-mf" className="text-2xl px-5 hover:underline">Be a MF</a>
-            <a href="#contact" className="ext-2xl px-5 hover:underline">Contact</a>
+            <a href="#story-section" className="text-2xl px-5 tracking-wide hover:underline scroll-link">MF’s Story</a>
+            <a href="#be-mf" className="text-2xl px-5 tracking-wide hover:underline">Be a MF</a>
+            <a href="#contact" className="ext-2xl px-5 tracking-wide hover:underline">Contact</a>
     </div>
   );
 }
