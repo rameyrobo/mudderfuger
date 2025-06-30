c2341b803c3fce9d22cf9e37f0c6284ef60dbd38
rameyrobo <rameyrobo@gmail.com>
2025-06-30 15:45:19 -0700

    Added CSS overrides and provided initial styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,17 +17,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={anton.className}>
       <head>
-        <link rel="preconnect" href="https://app.snipcart.com" />
-        <link rel="preconnect" href="https://cdn.snipcart.com" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.snipcart.com/themes/v3.6.1/default/snipcart.css"
-        />
-        <script
-          async
-          src="https://cdn.snipcart.com/themes/v3.6.1/default/snipcart.js"
-        ></script>
-      </head>
+  <link rel="preconnect" href="https://app.snipcart.com" />
+  <link rel="preconnect" href="https://cdn.snipcart.com" />
+  <link
+    rel="stylesheet"
+    href="https://cdn.snipcart.com/themes/v3.6.1/default/snipcart.css"
+  />
+  <link rel="stylesheet" href="/snipcart-overrides.css" />
+  <script
+    async
+    src="https://cdn.snipcart.com/themes/v3.6.1/default/snipcart.js"
+  ></script>
+</head>
       <body>
         {children}
         <SpeedInsights />
