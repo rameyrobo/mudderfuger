c617f295e513e47bf0d4cf32386096fcefb98999
Ramey Robles <rameyrobo@gmail.com>
2025-07-16 12:51:44 -0700

    Fixed variable error in resend environment variables

--- src/app/api/transactions/send-contact/route.ts
+++ src/app/api/transactions/send-contact/route.ts
@@ -4,13 +4,16 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-  const { name, email, message } = await request.json();
+  const { name, email, message, phone } = await request.json();
+
+  // Type assertion ensures sender is treated as a string
+  const sender = process.env.EMAIL_SENDER as string;
 
   const { data, error } = await resend.emails.send({
     from: `Mudderfuger <${sender}>`,
     to: [sender],
     subject: `Contact Form Submission from ${name}`,
-    html: `<strong>From:</strong> ${name} (${email})<br/><br/>${message}`,
+    html: `<strong>From:</strong> ${name} (${email})<br/><strong>Phone:</strong> ${phone || ''}<br/><br/>${message}`,
   });
 
   if (error) {
