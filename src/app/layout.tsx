b5f9ea75aa3b0c5a990de5b52c88b5a3f34a6e16
Ramey Robles <rameyrobo@gmail.com>
2025-08-11 15:49:34 -0700

    Removed snipcart from placeholder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Anton } from 'next/font/google'
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import LazyStylesheetLoader from '../components/LazyStylesheetLoader';
 
 
 export const metadata: Metadata = {
@@ -23,75 +22,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://mudderfuger.b-cdn.net" />
         <link rel="preconnect" href="https://app.snipcart.com" />
         <link rel="preconnect" href="https://cdn.snipcart.com" />
-        <script defer
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.SnipcartSettings = {
-                publicApiKey: "REDACTED_SNIPCART_KEY",
-                loadStrategy: "on-user-interaction",
-                loadCSS: false,
-                modalStyle: "side",
-                addProductBehavior: "none"
-              };
-
-              (function(){
-                var c,d;(d=(c=window.SnipcartSettings).version)!=null||(c.version="3.0");
-                var s,S;(S=(s=window.SnipcartSettings).timeoutDuration)!=null||(s.timeoutDuration=2750);
-                var l,p;(p=(l=window.SnipcartSettings).domain)!=null||(l.domain="cdn.snipcart.com");
-                var w,u;(u=(w=window.SnipcartSettings).protocol)!=null||(w.protocol="https");
-                var m,g;(g=(m=window.SnipcartSettings).loadCSS)!=null||(m.loadCSS=!0);
-                var y=window.SnipcartSettings.version?.includes("v3.0.0-ci")||window.SnipcartSettings.version!="3.0"&&window.SnipcartSettings.version.localeCompare("3.4.0",void 0,{numeric:!0,sensitivity:"base"})===-1,
-                f=["focus","mouseover","touchmove","scroll","keydown"];
-                window.LoadSnipcart=o;
-                document.readyState==="loading"?document.addEventListener("DOMContentLoaded",r):r();
-                function r(){window.SnipcartSettings.loadStrategy==="on-user-interaction"?(f.forEach(function(t){return document.addEventListener(t,o)}),setTimeout(o,window.SnipcartSettings.timeoutDuration)):o()}
-                var a=!1;
-                function o(){
-                  if(a)return;
-                  a=!0;
-                  let t=document.getElementsByTagName("head")[0],
-                      n=document.querySelector("#snipcart"),
-                      i=document.querySelector('src[src^="'+window.SnipcartSettings.protocol+"://"+window.SnipcartSettings.domain+'"][src$="snipcart.js"]'),
-                      e=document.querySelector('link[href^="'+window.SnipcartSettings.protocol+"://"+window.SnipcartSettings.domain+'"][href$="snipcart.css"]');
-                  n||(n=document.createElement("div"),n.id="snipcart",n.setAttribute("hidden","true"),document.body.appendChild(n)),h(n);
-                  i||(i=document.createElement("script"),i.src=window.SnipcartSettings.protocol+"://"+window.SnipcartSettings.domain+"/themes/v"+window.SnipcartSettings.version+"/default/snipcart.js",i.async=!0,t.appendChild(i));
-                  !e&&window.SnipcartSettings.loadCSS&&(e=document.createElement("link"),e.rel="stylesheet",e.type="text/css",e.href=window.SnipcartSettings.protocol+"://"+window.SnipcartSettings.domain+"/themes/v"+window.SnipcartSettings.version+"/default/snipcart.css",t.prepend(e));
-                  f.forEach(function(v){return document.removeEventListener(v,o)});
-                }
-                function h(t){
-                  !y||(t.dataset.apiKey=window.SnipcartSettings.publicApiKey,
-                  window.SnipcartSettings.addProductBehavior&&(t.dataset.configAddProductBehavior=window.SnipcartSettings.addProductBehavior),
-                  window.SnipcartSettings.modalStyle&&(t.dataset.configModalStyle=window.SnipcartSettings.modalStyle),
-                  window.SnipcartSettings.currency&&(t.dataset.currency=window.SnipcartSettings.currency),
-                  window.SnipcartSettings.templatesUrl&&(t.dataset.templatesUrl=window.SnipcartSettings.templatesUrl))
-                }
-              })();
-            `,
-          }}
-        ></script>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<link rel="stylesheet" href="/snipcart-overrides.css" />`
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-        const preloadLink = document.createElement('link');
-        preloadLink.rel = 'preload';
-        preloadLink.as = 'style';
-        preloadLink.href = '/snipcart-overrides.css';
-        preloadLink.fetchPriority = 'low';
-        preloadLink.onload = function () {
-          this.rel = 'stylesheet';
-        };
-        document.head.appendChild(preloadLink);
-      `,
-          }}
-        />
+        
 </head>
       <body>
-        <LazyStylesheetLoader />
         {children}
         <SpeedInsights />
         <div
